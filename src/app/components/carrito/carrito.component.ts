9092481a04143c7e9bdfd6817b2bddc7b2f40118
AngelLCortezDev <angellcortezdev@gmail.com>
2024-04-29 08:17:44 -0700

    Cambios en carrito

--- src/app/components/carrito/carrito.component.ts
+++ src/app/components/carrito/carrito.component.ts
@@ -22,4 +22,33 @@ export class CarritoComponent {
 
   }
 
+  updateUnits(operation: string, id: number) {
+
+    const product = this.servicioTienda.findProductById(id)
+    if (product) {
+      if (operation === 'minus' && product.cantidad > 0) {
+        product.cantidad = product.cantidad - 1;
+      }
+      if (operation === 'add') {
+        product.cantidad = product.cantidad + 1;
+
+      }
+      if (product.cantidad === 0) {
+        this.deleteProduct(id)
+      }
+    }
+
+  }
+  totalProduct(price: number, units: number) {
+    return price * units
+  }
+  deleteProduct(id: number) {
+    this.servicioTienda.deleteProduct(id);
+
+  }
+  totalCart() {
+    const result = this.servicioTienda.totalCart();
+    return result;
+  }
+
 }
